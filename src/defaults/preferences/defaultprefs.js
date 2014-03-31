06a4211ddce49232a66e2797133d7333642d2c1f
Robin Deits <robin.deits@gmail.com>
2014-03-30 23:51:29 -0400

    Working demo with tag-based search

--- src/defaults/preferences/defaultprefs.js
+++ src/defaults/preferences/defaultprefs.js
@@ -1,3 +1,4 @@
-pref("extensions.autozotbib.bibtex_filename", "/Users/robin/ZotOutput.bib");
+pref("extensions.autozotbib.bibtex_filename", "/Users/rdeits/ZotOutput.bib");
+pref("extensions.autozotbib.bibtex_tag", "CV");
 pref("extensions.autozotbib.automatic", true);
 pref("extensions.autozotbib.bibtex_translator_guid", "9cb70025-a888-4a29-a210-93ec52da40d4");
